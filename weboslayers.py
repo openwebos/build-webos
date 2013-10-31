7952f045874e1edad1574922b017851279154943
Susan Montooth <susan.montooth@lge.com>
2013-10-31 16:43:50 -0700

    webos-image: Update layer pins for 2013-10-31
    meta-webos as of 2013-10-30 22:56:39 -0700
    
    8a85c83 mtp: Add recipes for libmtp=v1.1.6 mtpfs=v1.1
    d8850cf webos-image: configurator=55
    
    Open-webOS-DCO-1.0-Signed-off-by: Susan Montooth <susan.montooth@lge.com>
    
    Change-Id: Ifb684bed3617e9f4f81381a48c2b3771e539c5c1
    Reviewed-on: https://g2g.palm.com/3758
    Reviewed-by: Build Verification
    Reviewed-by: Susan Montooth <susan.montooth@lge.com>
    Tested-by: Susan Montooth <susan.montooth@lge.com>

--- weboslayers.py
+++ weboslayers.py
@@ -60,5 +60,5 @@ webos_layers = [
 ('meta-networking',        6, 'git://github.com/openembedded/meta-oe.git',              '', ''),
 
 ('meta-webos-backports',   9, 'git://github.com/openwebos/meta-webos-backports.git',    'commit=7f21642', ''),
-('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=4bce8ea', ''),
+('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=8a85c83', ''),
 ]
