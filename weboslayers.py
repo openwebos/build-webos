db914c9cd0a0840fa91d827c7763a183129eda2a
Susan Montooth <susan.montooth@lge.com>
2013-11-04 15:17:25 -0800

    webos-image: Update layer pins for 2013-11-04
    meta-webos as of 2013-11-04 14:30:17 -0800
    
    e94ba6e webos-image: Enable tracepoints for luna-service2
    fa48892 webos-image: luna-service2=160
    ad12e90 webos-image: libpbnjson=24,v2.1.1
    
    Open-webOS-DCO-1.0-Signed-off-by: Susan Montooth <susan.montooth@lge.com>
    
    Change-Id: I831319d5ef88b8d76d64edcb2f83ab4a946af989
    Reviewed-on: https://g2g.palm.com/3781
    Build: Susan Montooth <susan.montooth@lge.com>
    Reviewed-by: Build Verification
    Reviewed-by: Susan Montooth <susan.montooth@lge.com>
    Tested-by: Susan Montooth <susan.montooth@lge.com>

--- weboslayers.py
+++ weboslayers.py
@@ -60,5 +60,5 @@ webos_layers = [
 ('meta-networking',        6, 'git://github.com/openembedded/meta-oe.git',              '', ''),
 
 ('meta-webos-backports',   9, 'git://github.com/openwebos/meta-webos-backports.git',    'commit=8700204', ''),
-('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=abf1555', ''),
+('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=e94ba6e', ''),
 ]
