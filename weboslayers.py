bb4714ac93918976cf2b676ab887a016d8840fa2
Susan Montooth <susan.montooth@lge.com>
2013-11-13 14:13:20 -0800

    webos-image: Update layer pins for 2013-11-13
    meta-webos as of 2013-11-13 13:20:38 -0800
    
    0ed46f5 webos-image: mojoservicelauncher=79
    
    Open-webOS-DCO-1.0-Signed-off-by: Susan Montooth <susan.montooth@lge.com>
    
    Change-Id: Iadf70136f3d48d9efcbc03dd788bd7c99dd042df
    Reviewed-on: https://g2g.palm.com/3862
    Build: Susan Montooth <susan.montooth@lge.com>
    Reviewed-by: Build Verification
    Reviewed-by: Susan Montooth <susan.montooth@lge.com>
    Tested-by: Susan Montooth <susan.montooth@lge.com>

--- weboslayers.py
+++ weboslayers.py
@@ -60,5 +60,5 @@ webos_layers = [
 ('meta-networking',        6, 'git://github.com/openembedded/meta-oe.git',              '', ''),
 
 ('meta-webos-backports',   9, 'git://github.com/openwebos/meta-webos-backports.git',    'commit=8700204', ''),
-('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=5827ad8', ''),
+('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=0ed46f5', ''),
 ]
