126e3ed9d06193809ffaf99da17c7d020b3682ff
Susan Montooth <susan.montooth@lge.com>
2013-12-06 14:55:45 -0800

    webos-image: Update layer pins for 2013-12-06
    meta-webos as of 2013-12-06 16:26:00 -0800
    
    0a4ded8 pmcertificatemgr: Add missing dependency on glib-2.0
    1fe91e9 webos-image: nyx-utils=14,v1.0.1
    3755398 icecc: Import improved version from oe-core master and
            always inherit
    de04d81 webos-image: luna-sysservice=26
    30c1a5a webos-image: pmcertificatemgr=29
    d401acc webos-image: alljoyn=v3.4.3
    
    Open-webOS-DCO-1.0-Signed-off-by: Susan Montooth <susan.montooth@lge.com>
    
    Change-Id: I4b2c318c2dd9dac8f52abf8855a254f10252d688
    Reviewed-on: https://g2g.palm.com/4166
    Reviewed-by: DCO Verification
    Build: Susan Montooth <susan.montooth@lge.com>
    Reviewed-by: Build Verification
    Reviewed-by: Susan Montooth <susan.montooth@lge.com>
    Tested-by: Susan Montooth <susan.montooth@lge.com>

--- weboslayers.py
+++ weboslayers.py
@@ -60,5 +60,5 @@ webos_layers = [
 ('meta-networking',        6, 'git://github.com/openembedded/meta-oe.git',              '', ''),
 
 ('meta-webos-backports',   9, 'git://github.com/openwebos/meta-webos-backports.git',    'commit=a11f27d', ''),
-('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=4623bff', ''),
+('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=0a4ded8', ''),
 ]
