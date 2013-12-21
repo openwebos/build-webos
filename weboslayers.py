4dcae391388f50fdd02542e719ff65a0b87c4e17
Martin Jansa <martin.jansa@lge.com>
2013-12-21 20:29:36 +0100

    webos-image: Update upstream layer pins for 2013-12-21
    bitbake as of 2013-12-03 17:12:25 +0000
    
    a779ae4 perforce: Fix path subdirectory issues
    f1e8223 fetch/hg: Improve user/password handling
    91e3735 fetch2/svn.py: use log instead of info to retrieve revision
    bbb4fa4 methodpool: Retire it, remove global method scope
    
    meta as of 2013-12-19 11:44:49 +0000
    
    394fa61 libsoup-2.4: add intltool-native to DEPENDS
    f79b2a6 build-appliance-image: Update to dylan head revision
    b17beeb build-appliance-image: Update to dylan head revision
    95f9889 libtiff: CVE-2013-4243
    96ae9c1 libtiff: CVE-2013-4232
    70f37e5 libtiff: fix CVE-2013-1960
    23f323b gst-ffmpeg: fix CVE-2013-3674
    4e31587 build-appliance-image: Update to dylan head revision
    05d0914 image-mklibs: ensure sysroot is correctly set when calling gcc
    f6bd8fc package.bbclass: ensure license excluded files aren't packaged
    32c064e license.bbclass: include all licenses in the manifest
    54a2cc3 license.bbclass: fix missing of license files on ubuntu build host
    9fdea10 metadata_scm: Avoid crashing on new svn version layouts
    43003ea db: Fix build with high PARALLEL_MAKE
    6dbad65 meta-toolchain-qt: put QT_CONF_PATH in environment script
    78710c0 nativesdk-qt4-tools: create qt.conf file
    da4eb72 lib/oe/path: Fix copytree functions for tar 1.27
    5e3ec61 classes: tar 1.27 fixes
    0e599ec dropbear: pass SFTPSERVER_PATH explicitly
    51008a2 elfutils-native: Update the patch to include the missing pieces needed for tests
    68e91f7 kern-tools: use dylan branch
    3549f5f busybox: configure system user id to range from 100 to 999
    5168f69 rpm: fix typo in PACKAGECONFIG
    eebd224 classes/package_rpm: fix bitbake package-index for RPM
    89a2988 findutils_4.2.31: backport fixes for doc build errors
    61efbcd elfutils-native: Fix build on distros with gcc 4.8
    d5478e1 kernel.bbclass: Correct post(inst|rm) package association
    8a00a57 python: Backport 2 CVE from upstream
    7dc11c4 perl: Backport 2 CVE Patches
    82b91d2 libxml2: remove patch for CVE-2012-2871
    d3393e2 sysvinit-inittab: ensure unique label for SERIAL_CONSOLES entries
    88c0274 ncurses: Fix problems expanding ncurses-libtinfo when in IMAGE_INSTALL
    280fab6 boost: fix build when PARALLEL_MAKE is not set
    0e4b296 mc: Don't remove libdir and split helpers into packages
    
    meta-oe as of 2013-10-10 21:30:38 +0200
    
    4475420 net-snmp: Backport fix for NETSNMP_NO_SYSTEMD typos
    70bc562 cherokee: fix SRC_URI
    a108b22 krb5: Add PACKAGECONFIG and new configure param to disable keyutils detection
    d014172 krb5: Backport patch to fix parallel build
    989f72c hexedit: new recipe
    
    :Issues Addressed:
    [ANG-339] Small update of upstream layers
    [ANG-340] CCC: Small update of upstream layers
    
    Open-webOS-DCO-1.0-Signed-off-by: Martin Jansa <martin.jansa@lge.com>
    Change-Id: Ib6797af3cb7a772638cb8a9ddc0e3c331295e7f0
    Reviewed-on: https://g2g.palm.com/4263
    Reviewed-by: DCO Verification
    Reviewed-by: Martin Jansa <Martin.Jansa@lge.com>
    Tested-by: Martin Jansa <Martin.Jansa@lge.com>
    Reviewed-by: Susan Montooth <susan.montooth@lge.com>

--- weboslayers.py
+++ weboslayers.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2008-2013 LG Electronics, Inc.
+# Copyright (c) 2008-2014 LG Electronics, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -54,9 +54,9 @@ Distribution = "webos"
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',               -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.18,commit=9313ff7', ''),
-('meta',                   5, 'git://github.com/openembedded/oe-core.git',              'branch=dylan,commit=a065c22', ''),
-('meta-oe',                6, 'git://github.com/openembedded/meta-oe.git',              'branch=dylan,commit=81bf02c', ''),
+('bitbake',               -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.18,commit=a779ae4', ''),
+('meta',                   5, 'git://github.com/openembedded/oe-core.git',              'branch=dylan,commit=394fa61', ''),
+('meta-oe',                6, 'git://github.com/openembedded/meta-oe.git',              'branch=dylan,commit=4475420', ''),
 ('meta-networking',        6, 'git://github.com/openembedded/meta-oe.git',              '', ''),
 
 ('meta-webos-backports',   9, 'git://github.com/openwebos/meta-webos-backports.git',    'commit=c53bc0c', ''),
