0ff106cfaf75d38258dd845b447c03ec97488cdf
Jongho Kim <jongho1208.kim@lge.com>
2013-12-27 22:10:57 +0900

    webos-image: Update layer pins for 2013-12-27
    meta-webos as of 2013-12-26 18:45:17 -0800
    
    0b9421a webos-image: alljoyn=v3.4.5
    
    Open-webOS-DCO-1.0-Signed-off-by: Jongho Kim <jongho1208.kim@lge.com>
    
    Change-Id: I3a327f06fc14f319ddd1ddc43292afd9f6973619
    Reviewed-on: https://g2g.palm.com/4286
    Reviewed-by: DCO Verification
    Reviewed-by: Build Verification
    Reviewed-by: Jongho Kim <jongho1208.kim@lge.com>
    Tested-by: Jongho Kim <jongho1208.kim@lge.com>

--- weboslayers.py
+++ weboslayers.py
@@ -60,5 +60,5 @@ webos_layers = [
 ('meta-networking',        6, 'git://github.com/openembedded/meta-oe.git',              '', ''),
 
 ('meta-webos-backports',   9, 'git://github.com/openwebos/meta-webos-backports.git',    'commit=c53bc0c', ''),
-('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=0a4286f', ''),
+('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=0b9421a', ''),
 ]
