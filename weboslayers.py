117028701d800a7fb158e6cfdef5ace06ed4b8a0
Susan Montooth <susan.montooth@lge.com>
2014-01-17 16:05:34 -0800

    webos-image: Update layer pins for 2014-01-17
    meta-webos as of 2014-01-17 13:54:49 -0800
    
    b64e7ff packagegroup-core-tools-profile: Fix parsing error
    049d438 packagegroup-core-tools-profile: Conditionalize LTTng packages via bbappend
    22d6a2e webos-preferred-providers.inc: Add kernel-vmlinux to BAD_RECOMMENDATIONS
    b36a78b webos-image: luna-sysservice=33
    9ac3a7c sstatesig: Allow to cross target-native/cross/nativesdk boundary
    53dce2e Allow world-read permissions for LS2 services
    
    Open-webOS-DCO-1.0-Signed-off-by: Susan Montooth <susan.montooth@lge.com>
    
    Change-Id: I21ad97ca710e9e21be3b088bdd107fd28acd4432
    Reviewed-on: https://g2g.palm.com/4425
    Reviewed-by: DCO Verification
    Build: Susan Montooth <susan.montooth@lge.com>
    Reviewed-by: Build Verification
    Reviewed-by: Susan Montooth <susan.montooth@lge.com>
    Tested-by: Susan Montooth <susan.montooth@lge.com>

--- weboslayers.py
+++ weboslayers.py
@@ -60,5 +60,5 @@ webos_layers = [
 ('meta-networking',        6, 'git://github.com/openembedded/meta-oe.git',              '', ''),
 
 ('meta-webos-backports',   9, 'git://github.com/openwebos/meta-webos-backports.git',    'commit=c53bc0c', ''),
-('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=2bd8a36', ''),
+('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=b64e7ff', ''),
 ]
