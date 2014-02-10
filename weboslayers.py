674e9f601bab779f6999a776d0ae16a89026b4e7
Susan Montooth <susan.montooth@lge.com>
2014-02-10 14:36:04 -0800

    webos-image: Update layer pins for 2014-02-10
    meta-webos as of 2014-02-10 00:22:47 -0800
    
    cba9cc2 nyx-modules: Add runtime dependency on gzip
    
    Open-webOS-DCO-1.0-Signed-off-by: Susan Montooth <susan.montooth@lge.com>
    
    Change-Id: I7fd90a2383c0f303f671adf7c4a7bc6fa165e0a3
    Reviewed-on: https://g2g.palm.com/4663
    Reviewed-by: DCO Verification
    Build: Susan Montooth <susan.montooth@lge.com>
    Reviewed-by: Build Verification
    Reviewed-by: Susan Montooth <susan.montooth@lge.com>
    Tested-by: Susan Montooth <susan.montooth@lge.com>

--- weboslayers.py
+++ weboslayers.py
@@ -60,5 +60,5 @@ webos_layers = [
 ('meta-networking',        6, 'git://github.com/openembedded/meta-oe.git',              '', ''),
 
 ('meta-webos-backports',   9, 'git://github.com/openwebos/meta-webos-backports.git',    'commit=c53bc0c', ''),
-('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=8eb313e', ''),
+('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=cba9cc2', ''),
 ]
