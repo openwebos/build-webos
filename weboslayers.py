17f4420fc31cf9d03a4e50f085c4c7d2940efec3
Susan Montooth <susan.montooth@lge.com>
2014-02-13 14:34:50 -0800

    webos-image: Update layer pins for 2014-02-13
    meta-webos as of 2014-02-13 14:26:05 -0800
    
    4728cd4 webos-image: pmlogdaemon=117
    598ad1e webos-image: libpbnjson=28,v2.4.0
    38707c8 webos-image: luna-service2=174
    5ae53b2 webos-image: libpbnjson=27,v2.3.0
    547bd30 webos-image: luna-service2=173,v3.6.0
    
    Open-webOS-DCO-1.0-Signed-off-by: Susan Montooth <susan.montooth@lge.com>
    
    Change-Id: Id252c4d26864e8726e0529d06db48e5d653601ad
    Reviewed-on: https://g2g.palm.com/4690
    Reviewed-by: DCO Verification
    Build: Susan Montooth <susan.montooth@lge.com>
    Reviewed-by: Build Verification
    Reviewed-by: Susan Montooth <susan.montooth@lge.com>
    Tested-by: Susan Montooth <susan.montooth@lge.com>

--- weboslayers.py
+++ weboslayers.py
@@ -60,5 +60,5 @@ webos_layers = [
 ('meta-networking',        6, 'git://github.com/openembedded/meta-oe.git',              '', ''),
 
 ('meta-webos-backports',   9, 'git://github.com/openwebos/meta-webos-backports.git',    'commit=c53bc0c', ''),
-('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=56a0805', ''),
+('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=4728cd4', ''),
 ]
