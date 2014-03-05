20592a7761c36c771cf4a7a61d0da06be5ae2505
Susan Montooth <susan.montooth@lge.com>
2014-03-05 15:11:21 -0800

    webos-image: Update layer pins for 2014-03-05
    meta-webos as of 2014-03-04 13:59:04 -0800
    
    08b8fe1 webos-image: luna-prefs=r11
    a0fb605 webos-image: Modify ROOTFS_READ_ONLY global variable handling
    4ac024c webos-image: exclude components from icecc build
    40a90ba icecc-create-env-native: Drop FILESPATH, document PATCHTOOL
    184bbf2 webos-image: icecc-create-env=1.0.1
    ac93885 webos-image: luna-sysservice=37
    83dfffe webos-image: db8=128
    
    Open-webOS-DCO-1.0-Signed-off-by: Susan Montooth <susan.montooth@lge.com>
    
    Change-Id: I949a79e113baeccf4bd13c51c5333aca561ea4a4
    Reviewed-on: https://g2g.palm.com/4851
    Reviewed-by: DCO Verification
    Build: Susan Montooth <susan.montooth@lge.com>
    Reviewed-by: Build Verification
    Reviewed-by: Susan Montooth <susan.montooth@lge.com>
    Tested-by: Susan Montooth <susan.montooth@lge.com>

--- weboslayers.py
+++ weboslayers.py
@@ -60,5 +60,5 @@ webos_layers = [
 ('meta-networking',        6, 'git://github.com/openembedded/meta-oe.git',              '', ''),
 
 ('meta-webos-backports',   9, 'git://github.com/openwebos/meta-webos-backports.git',    'commit=c53bc0c', ''),
-('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=09a428f', ''),
+('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=08b8fe1', ''),
 ]
