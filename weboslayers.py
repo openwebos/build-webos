b3cf2e7520b9ca1ca0004c98dff83dfe7f8fe277
Susan Montooth <susan.montooth@lge.com>
2014-03-06 15:30:25 -0800

    webos-image: Update upstream layer pins for 2014-03-06
    meta as of 2014-03-06 10:31:45 +0000
    
    fae45ab gnutls: fix failure during do_compile
    2d6d738 gnutls: Fixed bug that prevented the rejection of v1 intermediate CA certificates.
    d622439 gnutls: CVE-2014-0092 correct return codes
    
    :Issues Addressed:
    [GF-61858] GnuTLS security vulnerability
    
    Open-webOS-DCO-1.0-Signed-off-by: Susan Montooth <susan.montooth@lge.com>
    
    Change-Id: I1886d38667bd5bb67749e287bc8db6a00b7e3e15
    Reviewed-on: https://g2g.palm.com/4864
    Reviewed-by: DCO Verification
    Build: Susan Montooth <susan.montooth@lge.com>
    Reviewed-by: Build Verification
    Reviewed-by: Susan Montooth <susan.montooth@lge.com>
    Tested-by: Susan Montooth <susan.montooth@lge.com>

--- weboslayers.py
+++ weboslayers.py
@@ -55,7 +55,7 @@ Distribution = "webos"
 # repositories on git.openembedded.org
 webos_layers = [
 ('bitbake',               -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.18,commit=66d0133', ''),
-('meta',                   5, 'git://github.com/openembedded/oe-core.git',              'branch=dylan,commit=4223278', ''),
+('meta',                   5, 'git://github.com/openembedded/oe-core.git',              'branch=dylan,commit=fae45ab', ''),
 ('meta-oe',                6, 'git://github.com/openembedded/meta-oe.git',              'branch=dylan,commit=cb18201', ''),
 ('meta-networking',        6, 'git://github.com/openembedded/meta-oe.git',              '', ''),
 
