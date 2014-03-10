d062fe202855f5d983572ba0b80bf2e278223c6f
Susan Montooth <susan.montooth@lge.com>
2014-03-09 21:43:15 -0700

    webos-image: Update layer pins for 2014-03-09
    meta-webos as of 2014-03-09 21:38:30 -0700
    
    f42e462 Revert "webos-image: nodejs=v0.10.25"
    
    Open-webOS-DCO-1.0-Signed-off-by: Susan Montooth <susan.montooth@lge.com>
    
    Change-Id: Ie4cf325c531edba61b267b72738f44b8b639f53b
    Reviewed-on: https://g2g.palm.com/4883
    Reviewed-by: DCO Verification
    Build: Susan Montooth <susan.montooth@lge.com>
    Reviewed-by: Build Verification
    Reviewed-by: Susan Montooth <susan.montooth@lge.com>
    Tested-by: Susan Montooth <susan.montooth@lge.com>

--- weboslayers.py
+++ weboslayers.py
@@ -60,5 +60,5 @@ webos_layers = [
 ('meta-networking',        6, 'git://github.com/openembedded/meta-oe.git',              '', ''),
 
 ('meta-webos-backports',   9, 'git://github.com/openwebos/meta-webos-backports.git',    'commit=c53bc0c', ''),
-('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=70db0c8', ''),
+('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=f42e462', ''),
 ]
