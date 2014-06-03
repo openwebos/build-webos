bc75fc6a4b728ba1539b7c3dc9fc42d032ae9873
Susan Montooth <susan.montooth@lge.com>
2014-06-03 10:37:47 -0700

    webos-image: Update layer pins for 2014-06-03
    meta-webos as of 2014-06-03 10:35:20 -0700
    
    28c6805 webos_enhanced_submissions: Fix various SRCREV sanity check failures
    302ad4a webos-image: libpalmsocket=32
    f00c3e8 sqlcipher: don't build with TCL support
    41f6653 webos-image: libpbnjson=36,v2.8.0
    58dd24b webos-image: gnutls=v2.12.23-0webos1
    7f507b5 webos-image: libgcrypt=v1.5.0-0webos1
    470b14d webos-image: curl=v7.29.0-0webos1
    
    Open-webOS-DCO-1.0-Signed-off-by: Susan Montooth <susan.montooth@lge.com>
    
    Change-Id: Idf23c40bb506959d6d4214835fa97ca63afbd1b1
    Reviewed-on: https://g2g.palm.com/5887
    Reviewed-by: DCO Verification
    Build: Susan Montooth <susan.montooth@lge.com>
    Reviewed-by: Build Verification
    Reviewed-by: Susan Montooth <susan.montooth@lge.com>
    Tested-by: Susan Montooth <susan.montooth@lge.com>

--- weboslayers.py
+++ weboslayers.py
@@ -60,5 +60,5 @@ webos_layers = [
 ('meta-networking',        6, 'git://github.com/openembedded/meta-oe.git',              '', ''),
 
 ('meta-webos-backports',   9, 'git://github.com/openwebos/meta-webos-backports.git',    'commit=907a089', ''),
-('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=f4b0289', ''),
+('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=28c6805', ''),
 ]
