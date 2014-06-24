e94a7be5c1d346a68dae80ddab575c545707012e
Susan Montooth <susan.montooth@lge.com>
2014-06-24 16:28:57 -0700

    webos-image: Update layer pins for 2014-06-24
    meta-webos as of 2014-06-24 16:24:53 -0700
    
    1deb1a7 webos-image: nyx-utils=17,v1.1.2
    
    Open-webOS-DCO-1.0-Signed-off-by: Susan Montooth <susan.montooth@lge.com>
    
    Change-Id: If7e9643905fec4e2326e6efd2712880053d15535
    Reviewed-on: https://g2g.palm.com/6099
    Reviewed-by: DCO Verification
    Build: Susan Montooth <susan.montooth@lge.com>
    Reviewed-by: Build Verification
    Reviewed-by: Susan Montooth <susan.montooth@lge.com>
    Tested-by: Susan Montooth <susan.montooth@lge.com>

--- weboslayers.py
+++ weboslayers.py
@@ -60,5 +60,5 @@ webos_layers = [
 ('meta-networking',        6, 'git://github.com/openembedded/meta-oe.git',              '', ''),
 
 ('meta-webos-backports',   9, 'git://github.com/openwebos/meta-webos-backports.git',    'commit=907a089', ''),
-('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=81282a6', ''),
+('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=1deb1a7', ''),
 ]
