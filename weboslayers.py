e4f304b22a48bd7827086cc302e53ba6071ffecb
Susan Montooth <susan.montooth@lge.com>
2014-08-18 14:07:40 -0700

    webos-image: Update layer pins for 2014-08-18
    meta-webos as of 2014-08-18 14:05:59 -0700
    
    ee137f6 webos-image: db8=145
    1c95735 webos-image: pmloglib=54,v3.2.0
    20b3bb0 webos-image: hdparm=webos2
    bf0b3ff e2fsprogs: Add missing colon in FILESEXTRAPATHS
    3905a71 recipes: Add missing pkgconfig inherit
    54a0564 webos_kernel_dep.bbclass: Add virtual/kernel dependency
    d43f9fa prepare-submission.sh: Add addressed issues in message (v1.0.3)
    f3f058d prepare-submission.sh: Add --no-merges option to git log (v1.0.2)
    b6f2111 recipes: Remove RREPLACES and RCONFLICTS for old component names
    2f7cf6e webos-image: libpbnjson=39,v2.9.1
    f10526e webos-image: db8=144
    a23e4d3 webos-image: luna-sysservice=39 luna-service2=196
    195cc96 webos-image: lighttpd=v1.4.32-0webos2
    cd8ffbb webos-image: libmms=0webos1
    cbc5a6b webos-image: nodejs=r7
    
    Open-webOS-DCO-1.0-Signed-off-by: Susan Montooth <susan.montooth@lge.com>
    
    Change-Id: Ic4a98ab613c880282ebbe15de6123e021f339f4d
    Reviewed-on: https://g2g.palm.com/6438
    Reviewed-by: DCO Verification
    Build: Susan Montooth <susan.montooth@lge.com>
    Reviewed-by: Build Verification
    Reviewed-by: Susan Montooth <susan.montooth@lge.com>
    Tested-by: Susan Montooth <susan.montooth@lge.com>

--- weboslayers.py
+++ weboslayers.py
@@ -64,5 +64,5 @@ webos_layers = [
 ('meta-networking',        6, 'git://github.com/openembedded/meta-oe.git',              '', ''),
 
 ('meta-webos-backports',   9, 'git://github.com/openwebos/meta-webos-backports.git',    'commit=ed80399', ''),
-('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=2a57c54', ''),
+('meta-webos',            10, 'git://github.com/openwebos/meta-webos.git',              'commit=ee137f6', ''),
 ]
